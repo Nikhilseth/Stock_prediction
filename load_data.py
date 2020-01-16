bd92b6d48aaf900c856d359432549ec906baba75
Nik Seth <nik@emojihealth.com>
2020-01-15 21:01:59 -0500

    updated plotting, adding windowing

--- load_data.py
+++ load_data.py
@@ -6,17 +6,20 @@ Created on Sat Jan 11 13:27:12 2020
 """
 
 import os
+import numpy as np
 import pandas as pd
 import random
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.io as pio
+pio.renderers.default = 'browser'
 
 data_path = r'.\Dataset\Stocks'
 # data_path = os.path.expanduser("~/Documents/Github/Dataset/Stocks") #for Mac
 filenames = [os.path.join(data_path,f) for f in os.listdir(data_path) if f.endswith('.txt') and os.path.getsize(os.path.join(data_path,f)) > 0]
 
+filenames = random.sample(filenames,5)
 data = []
-plt.figure()
-for file in filenames[:10]:
+for file in filenames:
     
     df = pd.read_csv(file)
     
@@ -24,10 +27,51 @@ for file in filenames[:10]:
     df['Date'] = pd.to_datetime(df['Date'])
     data.append(df)
     
-    #plot visualization
-    df['Close'].plot(style='--',label=df['Label'][0])
+
+#%% plot sample of data    
+r = lambda: random.randint(0,255)
+traces = []
+
+for df in data:
+    clr_str = 'rgb('+str(r())+','+str(r())+','+str(r())+')'
     
-plt.title('Sample of Stock Data')
-plt.xlabel('Time')
-plt.ylabel('Closing Price')
-plt.legend()
+    df = df.sort_values('Date')
+    label = df['Label'].iloc[0]
+    
+    trace = go.Scattergl(
+        x=df['Date'],
+        y=df['Close'],
+        mode='lines',
+        line=dict(color=clr_str),
+        name=label)
+    traces.append(trace)
+    
+layout = go.Layout(title='Sample plot of Stocks')
+fig = go.Figure(data=traces,layout=layout)
+fig.update_xaxes(title_text='Date')
+fig.update_yaxes(title_text='Close Price')
+fig.show()
+
+#%% create windows
+df = data[0]
+window_len = 10
+
+#split train and test set
+split_date = list(data[0]['Date'][-(2*window_len+1):])[0]
+training_set, test_set = df[df['Date'] < split_date], df[df['Date'] >= split_date]
+training_set = training_set.drop(['Date','Label','OpenInt'],1)
+test_set = test_set.drop(['Date','Label','OpenInt'],1)
+
+#create training windows
+LSTM_training_inputs = []
+for i in range(len(training_set)-window_len):
+    temp_set = training_set[i:(i+window_len)].copy()
+    
+    for col in list(temp_set):
+        temp_set[col] = temp_set[col]/temp_set[col].iloc[0]-1
+        
+    LSTM_training_inputs.append(temp_set)
+LSTM_training_outputs = (training_set['Close'][window_len:].values/training_set['Close'][:-window_len].values)-1
+
+LSTM_training_inputs = [np.array(LSTM_training_input) for LSTM_training_input in LSTM_training_inputs]
+LSTM_training_inputs = np.array(LSTM_training_inputs)
